74a1da8c32a2595231aa86eec8afb8f34cf742ad
RandlD19 <57182940+RandlD19@users.noreply.github.com>
2021-05-30 22:51:40 +0200

    Update Teren_raven.java

--- TankWar/src/Teren_raven.java
+++ TankWar/src/Teren_raven.java
@@ -1,4 +1,5 @@
 public class Teren_raven extends Objekt {
+	// Razred Teren_raven je podrazred razreda Objekt, ki predstavlja teren na katerem se peljeta tanka. Sprejme polozaj razreda Polozaj
 
 	public Teren_raven(Polozaj polozaj) {
 		super(polozaj);
