0438dd843f7c9cf81b46576227b0b7990de2889b
RandlD19 <57182940+RandlD19@users.noreply.github.com>
2021-05-30 18:58:41 +0200

    Update IgralnoPolje.java

--- TankWar/src/IgralnoPolje.java
+++ TankWar/src/IgralnoPolje.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 
 public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionListener {
-
+	// razred IgralnoPolje implementira grafiko igrice TankWar
 	private CardLayout cl;
 	public Zazeni zazeni;
 
@@ -38,17 +38,17 @@ public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionList
 
 	public IgralnoPolje(CardLayout cl, Zazeni zazeni) {
         super();
-
-    
-		this.cl = cl;
-		this.zazeni = zazeni;
+		
+	this.cl = cl;
+	this.zazeni = zazeni;
 
         tank1 = zazeni.tank1;
         tank2 = zazeni.tank2;
         eksplozija = zazeni.eksplozija;
         
-		if (zazeni.igralec == 1) {
-        	trenutniTank = tank1;
+	// ustvarimo nov objek tanka, ki predstavlja tank, ki je trenutno na potezi 
+	if (zazeni.igralec == 1) {
+        	trenutniTank = tank1; 
         }
         else {
         	trenutniTank = tank2;
@@ -57,48 +57,47 @@ public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionList
         setBackground(Color.pink);
         setFocusable(true);
         addMouseMotionListener(this);		
-		addMouseListener(new MouseInputAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {			
-			}
-			
-			public void mousePressed(MouseEvent e) {
-				
-				zazeni.eksplozija = null;
-				cas = 0;
-				povecujCas = true;
-				x_miske = e.getX();
-				y_miske = e.getY();
-				
-//				trenutni_cas = 0;
-			
-				repaint();
-				
-				
-			}
-			
-			public void mouseReleased(MouseEvent e) {
+	addMouseListener(new MouseInputAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {			
+		}
 
-				povecujCas = false;
-				
-				razdalja = (int)(Math.sqrt(Math.pow((x_miske - (int)trenutniTank.getPolozaj().x), 2.0) + Math.pow((y_miske - (int)trenutniTank.getPolozaj().y), 2.0)));
-				
-				int x = (int)(((double)(x_miske - (int)trenutniTank.getPolozaj().x)/(double)razdalja)*(double)(cas));
-				int y = (int)(((double)(y_miske - (int)trenutniTank.getPolozaj().y)/(double)razdalja)*(double)(cas));
+		public void mousePressed(MouseEvent e) {
+
+			zazeni.eksplozija = null; // trenutna eksplozija se odstrani
+			cas = 0;
+			povecujCas = true; // s pomocjo stevca cas nastavimo moč izstrelitve krogle
+			x_miske = e.getX();
+			y_miske = e.getY();
+
+			repaint();
+
+
+		}
+
+		public void mouseReleased(MouseEvent e) {
+
+			povecujCas = false;
+			// poiscemo razdaljo med tankom in klikom miske 
+			razdalja = (int)(Math.sqrt(Math.pow((x_miske - (int)trenutniTank.getPolozaj().x), 2.0) + Math.pow((y_miske - (int)trenutniTank.getPolozaj().y), 2.0)));
 			
-				
-				int x_tank = (int)trenutniTank.getPolozaj().x + (trenutniTank.getPolozaj().dolzina / 2);
-				int y_tank = (int)trenutniTank.getPolozaj().y + (trenutniTank.getPolozaj().visina / 3);
-				
-				zazeni.krogla = new Krogla(new Polozaj(x_tank, y_tank-20, 15,15), x, y, 0, 0);
-				zazeni.eksplozija = null;
-				
-				cas = 0;
-				repaint();
-			}
-		
+			// izracunamo hitrost v x in y smeri na podlagi dolzine casa, ko smo drzali klik miske
+			int x = (int)(((double)(x_miske - (int)trenutniTank.getPolozaj().x)/(double)razdalja)*(double)(cas));
+			int y = (int)(((double)(y_miske - (int)trenutniTank.getPolozaj().y)/(double)razdalja)*(double)(cas));
 			
-		});
+			int x_tank = (int)trenutniTank.getPolozaj().x + (trenutniTank.getPolozaj().dolzina / 2);
+			int y_tank = (int)trenutniTank.getPolozaj().y + (trenutniTank.getPolozaj().visina / 3);
+			
+			// izstrelimo kroglo, ki ji nastavimo zacetni polozaj ter zacetno hitrost v x in y smeri
+			zazeni.krogla = new Krogla(new Polozaj(x_tank, y_tank-20, 15,15), x, y, 0, 0);
+			zazeni.eksplozija = null;
+
+			cas = 0; // ponastavimo stevec casa
+			repaint();
+		}
+
+
+	});
 		
 		
 		
@@ -111,7 +110,7 @@ public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionList
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
+		// sprotno pridobivamo polozaj miske na oknu, saj ta določa smer leta krogle
 		x_miske_premik = e.getX();
 		y_miske_premik = e.getY();
 		
@@ -123,114 +122,116 @@ public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionList
 	
 	@Override
     public void paint(Graphics g) {
-        super.paint(g); // klic metode nadrazreda
-        Graphics2D liki = (Graphics2D)g; // pretvarjanje tipov
+        super.paint(g); 
+        Graphics2D liki = (Graphics2D)g; 
 
         liki.setColor(Color.BLACK);
         
+	// s pomocjo faktorjev dolzina in visina nastavljamo dolzino in sirino elementov na igralnem polju v primeru razširitve oz zožitve okna
         double dolzina = (double)(getWidth())/(double)(zazeni.dolzina);
         double visina = (double)(getHeight())/(double)(zazeni.visina);
         
         liki.setFont(new Font(Font.SANS_SERIF, Font.BOLD|Font.ITALIC, 50));
      
-        
+        // ko enemu izmed tankov zmanjka zivljenj izpisemo spodnji napis in zakljucimo igro
         if (trenutniTank == zazeni.tank1) {
-			if (zazeni.tank1.zivljenja == 0) {
-				liki.drawString("IGRALEC 2 JE ZMAGOVALEC!", (int)(5*(getWidth()/32) * dolzina), (int)(getHeight()/4 * visina));
-				ustavi = true;
-			}
+		if (zazeni.tank1.zivljenja == 0) {
+			liki.drawString("IGRALEC 2 JE ZMAGOVALEC!", (int)(5*(getWidth()/32) * dolzina), (int)(getHeight()/4 * visina));
+			ustavi = true;
+		}
 		}
 			
-		else {
-			if (zazeni.tank2.zivljenja == 0) {
-				liki.drawString("IGRALEC 1 JE ZMAGOVALEC!",  (int)(5*(getWidth()/32) * dolzina), (int)(getHeight()/4 * visina));
-				ustavi = true;
-			}
+	else {
+		if (zazeni.tank2.zivljenja == 0) {
+			liki.drawString("IGRALEC 1 JE ZMAGOVALEC!",  (int)(5*(getWidth()/32) * dolzina), (int)(getHeight()/4 * visina));
+			ustavi = true;
 		}
+	}
         liki.setStroke(new BasicStroke(2.0f));
         
-		liki.drawPolygon(new int[] {(int)(50*dolzina), (int)((50+775*0.2)*dolzina), (int)((50+775*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-775*0.02)*visina), (int)(50*visina)}, 3);
-		liki.drawPolygon(new int[] {(int)((800)*dolzina), (int)(((800)+775*0.2)*dolzina), (int)(((800)+775*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-775*0.02)*visina), (int)(50*visina)}, 3);
+	// narisemo kazalec moči 
+	liki.drawPolygon(new int[] {(int)(50*dolzina), (int)((50+775*0.2)*dolzina), (int)((50+775*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-775*0.02)*visina), (int)(50*visina)}, 3);
+	liki.drawPolygon(new int[] {(int)((800)*dolzina), (int)(((800)+775*0.2)*dolzina), (int)(((800)+775*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-775*0.02)*visina), (int)(50*visina)}, 3);
 
-		liki.setStroke(new BasicStroke(1.0f));
-		
+	liki.setStroke(new BasicStroke(1.0f));
+
         liki.setColor(Color.BLACK);
         
         liki.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
 		
-		liki.drawString("Igralec 1", (int)(50*dolzina), (int)(20*visina));
-		liki.drawString("Igralec 2", (int)(getWidth() - 160*dolzina), (int)(20*visina));
-		
-		
-		
-		for (int i = 1 ; i < zazeni.tank1.zivljenja + 1; i++) {
-			liki.drawString("\u2665", (int)(150*dolzina) + (int)(i*20*dolzina), (int)(20*visina));
-		}
-		
-		for (int i = 1 ; i < zazeni.tank2.zivljenja + 1; i++) {
-			liki.drawString("\u2665", (int)(getWidth() - 190*dolzina) - (int)(i*20*dolzina), (int)(20*visina));
-		}
-		
-		
-		
-		
-        
-		liki.drawImage(tank1.slika, (int)Math.round((tank1.getPolozaj().getX())*dolzina), (int)Math.round((tank1.getPolozaj().getY())*visina), (int)Math.round((tank1.getPolozaj().getDolzina())*dolzina),(int)Math.round((tank1.getPolozaj().getVisina())*visina), null);
-		liki.drawImage(tank2.slika, (int)Math.round((tank2.getPolozaj().getX())*dolzina), (int)Math.round((tank2.getPolozaj().getY())*visina), (int)Math.round((tank2.getPolozaj().getDolzina())*dolzina),(int)Math.round((tank2.getPolozaj().getVisina())*visina), null);
-		
-		liki.drawRect((int)Math.round(zazeni.teren1.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren1.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren1.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren1.getPolozaj().getVisina()*visina));
-		liki.fillRect((int)Math.round(zazeni.teren1.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren1.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren1.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren1.getPolozaj().getVisina()*visina));
+	liki.drawString("Igralec 1", (int)(50*dolzina), (int)(20*visina));
+	liki.drawString("Igralec 2", (int)(getWidth() - 160*dolzina), (int)(20*visina));
+	
+	// narisemo srca, ki predstavljajo stevilo zivljenj za posamezni tank
+	for (int i = 1 ; i < zazeni.tank1.zivljenja + 1; i++) {
+		liki.drawString("\u2665", (int)(150*dolzina) + (int)(i*20*dolzina), (int)(20*visina));
+	}
 
-		liki.drawRect((int)Math.round(zazeni.teren2.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren2.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren2.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren2.getPolozaj().getVisina()*visina));
-		liki.fillRect((int)Math.round(zazeni.teren2.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren2.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren2.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren2.getPolozaj().getVisina()*visina));
+	for (int i = 1 ; i < zazeni.tank2.zivljenja + 1; i++) {
+		liki.drawString("\u2665", (int)(getWidth() - 190*dolzina) - (int)(i*20*dolzina), (int)(20*visina));
+	}
 
-		liki.fillOval((int)Math.round((zazeni.hrib1.getPolozaj().getX())*dolzina), (int)Math.round((zazeni.hrib1.getPolozaj().getY())*visina), (int)Math.round((zazeni.hrib1.getPolozaj().getDolzina())*dolzina), (int)Math.round((zazeni.hrib1.getPolozaj().getVisina())*visina));
-		liki.fillOval((int)Math.round((zazeni.hrib2.getPolozaj().getX())*dolzina), (int)Math.round((zazeni.hrib2.getPolozaj().getY())*visina), (int)Math.round((zazeni.hrib2.getPolozaj().getDolzina())*dolzina), (int)Math.round((zazeni.hrib2.getPolozaj().getVisina())*visina));
-		
-		if (zazeni.eksplozija != null) {
-		liki.drawImage(zazeni.eksplozija.slika, (int)Math.round((zazeni.eksplozija.getPolozaj().getX())*dolzina), (int)Math.round((zazeni.eksplozija.getPolozaj().getY())*visina), (int)Math.round((zazeni.eksplozija.getPolozaj().getDolzina())*dolzina),(int)Math.round((zazeni.eksplozija.getPolozaj().getVisina())*visina), null);
-		}
-		
-		if (zazeni.igralec == 1) {
-	    	trenutniTank = tank1;
-	    }
-	    else {
-	    	trenutniTank = tank2;
-	    }
-		
-		int x_tank = (int)trenutniTank.getPolozaj().x + (trenutniTank.getPolozaj().dolzina / 2);
-		int y_tank = (int)trenutniTank.getPolozaj().y + (trenutniTank.getPolozaj().visina / 3);
-		
-		double x = (x_miske_premik - x_tank)*dolzina;
-		double y = (y_miske_premik - y_tank)*visina;
-		
-		double h = Math.sqrt(y*y + x*x);
-		double d = 25;
-		
-		
-		double x_koncna = (x_tank + (d*x)/h);
-		double y_koncna = (y_tank + (d*y)/h);
-		
-		if (trenutniTank == tank1) {
-			liki.fillPolygon(new int[] {(int)(50*dolzina), (int)((50+cas*0.2)*dolzina), (int)((50+cas*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-cas*0.02)*visina), (int)(50*visina)}, 3);
+	// narisemo tanka na zacetno pozicijo
+	liki.drawImage(tank1.slika, (int)Math.round((tank1.getPolozaj().getX())*dolzina), (int)Math.round((tank1.getPolozaj().getY())*visina), (int)Math.round((tank1.getPolozaj().getDolzina())*dolzina),(int)Math.round((tank1.getPolozaj().getVisina())*visina), null);
+	liki.drawImage(tank2.slika, (int)Math.round((tank2.getPolozaj().getX())*dolzina), (int)Math.round((tank2.getPolozaj().getY())*visina), (int)Math.round((tank2.getPolozaj().getDolzina())*dolzina),(int)Math.round((tank2.getPolozaj().getVisina())*visina), null);
+	
+	// narisemo ozadje (ravnino in hriba)
+	liki.drawRect((int)Math.round(zazeni.teren1.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren1.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren1.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren1.getPolozaj().getVisina()*visina));
+	liki.fillRect((int)Math.round(zazeni.teren1.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren1.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren1.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren1.getPolozaj().getVisina()*visina));
 
-		}
-		else {
-			liki.fillPolygon(new int[] {(int)((800)*dolzina), (int)(((800)+cas*0.2)*dolzina), (int)(((800)+cas*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-cas*0.02)*visina), (int)(50*visina)}, 3);
+	liki.drawRect((int)Math.round(zazeni.teren2.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren2.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren2.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren2.getPolozaj().getVisina()*visina));
+	liki.fillRect((int)Math.round(zazeni.teren2.getPolozaj().getX()*dolzina), (int)Math.round((zazeni.teren2.getPolozaj().getY())*visina), (int)Math.round(zazeni.teren2.getPolozaj().getDolzina()*dolzina), (int)Math.round(zazeni.teren2.getPolozaj().getVisina()*visina));
 
-		}
-		
+	liki.fillOval((int)Math.round((zazeni.hrib1.getPolozaj().getX())*dolzina), (int)Math.round((zazeni.hrib1.getPolozaj().getY())*visina), (int)Math.round((zazeni.hrib1.getPolozaj().getDolzina())*dolzina), (int)Math.round((zazeni.hrib1.getPolozaj().getVisina())*visina));
+	liki.fillOval((int)Math.round((zazeni.hrib2.getPolozaj().getX())*dolzina), (int)Math.round((zazeni.hrib2.getPolozaj().getY())*visina), (int)Math.round((zazeni.hrib2.getPolozaj().getDolzina())*dolzina), (int)Math.round((zazeni.hrib2.getPolozaj().getVisina())*visina));
 
-		liki.setStroke(new BasicStroke(4.0f));
-		
-		liki.setColor(new Color (78,82,66));
-		
-		liki.drawLine((int)(x_tank*dolzina), (int)(y_tank*visina), (int)(x_koncna*dolzina), (int)(y_koncna*visina));
-		
-		liki.setColor(Color.RED);
-		if (zazeni.krogla != null) {
-			liki.fillOval((int)(zazeni.krogla.x*dolzina), (int)(zazeni.krogla.y*visina), (int)(15*dolzina), (int)(15*visina));
-		}
+	// izrisemo eksplozijo na polozaj, kjer je krogla trčila z drugim objekom
+	if (zazeni.eksplozija != null) {
+	liki.drawImage(zazeni.eksplozija.slika, (int)Math.round((zazeni.eksplozija.getPolozaj().getX())*dolzina), (int)Math.round((zazeni.eksplozija.getPolozaj().getY())*visina), (int)Math.round((zazeni.eksplozija.getPolozaj().getDolzina())*dolzina),(int)Math.round((zazeni.eksplozija.getPolozaj().getVisina())*visina), null);
+	}
+
+	if (zazeni.igralec == 1) {
+	trenutniTank = tank1;
+    	}
+    	else {
+		trenutniTank = tank2;
+    	}
+
+	int x_tank = (int)trenutniTank.getPolozaj().x + (trenutniTank.getPolozaj().dolzina / 2);
+	int y_tank = (int)trenutniTank.getPolozaj().y + (trenutniTank.getPolozaj().visina / 3);
+
+	double x = (x_miske_premik - x_tank)*dolzina;
+	double y = (y_miske_premik - y_tank)*visina;
+
+	double h = Math.sqrt(y*y + x*x);
+	double d = 25; // dolzina cevi, ki kaže smer izstrelka
+
+	double x_koncna = (x_tank + (d*x)/h);
+	double y_koncna = (y_tank + (d*y)/h);
+	
+	// v prej okvirjen trikotnik narisemo dejansko moč izstrelka 
+	if (trenutniTank == tank1) {
+		liki.fillPolygon(new int[] {(int)(50*dolzina), (int)((50+cas*0.2)*dolzina), (int)((50+cas*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-cas*0.02)*visina), (int)(50*visina)}, 3);
+
+	}
+	else {
+		liki.fillPolygon(new int[] {(int)((800)*dolzina), (int)(((800)+cas*0.2)*dolzina), (int)(((800)+cas*0.2)*dolzina)}, new int[] {(int)(50*visina), (int)((50-cas*0.02)*visina), (int)(50*visina)}, 3);
+
+	}
+
+
+	liki.setStroke(new BasicStroke(4.0f));
+
+	liki.setColor(new Color (78,82,66));
+	// narisemo top tanka
+	liki.drawLine((int)(x_tank*dolzina), (int)(y_tank*visina), (int)(x_koncna*dolzina), (int)(y_koncna*visina));
+
+	    
+	// narisemo kroglo
+	liki.setColor(Color.RED);
+	if (zazeni.krogla != null) {
+		liki.fillOval((int)(zazeni.krogla.x*dolzina), (int)(zazeni.krogla.y*visina), (int)(15*dolzina), (int)(15*visina));
+	}
 		
 		
 		
@@ -241,7 +242,9 @@ public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionList
 
 	}
 
+	
 	@Override
+	// s puscicama levo in desno lahko premikamo tank
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode()== KeyEvent.VK_RIGHT) {
 
@@ -253,7 +256,7 @@ public class IgralnoPolje extends JPanel implements KeyListener, MouseMotionList
 			trenutniTank.setPremik_v_levo(true);
 
 		}
-		zazeni.eksplozija = null;
+		zazeni.eksplozija = null; // ce tank prestavimo eksplozija izgine
 		repaint();
 	}
 
